6739f74480f0dafce24e550b5ac00cad43bf371f
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-10-20 07:04:53 -0500

    conexión mysql exitosa y se adiciona el primer enrutador

--- backend-mi-proyecto-ppi/db/mysql.js
+++ backend-mi-proyecto-ppi/db/mysql.js
@@ -1,7 +1,18 @@
 const mysql      = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'root',
+  password : '',
+  database : 'media_tecnica_app'
 });
+
+connection.connect((error) => {
+    if(error){
+      console.log(`Error en conexión a base de datos: ${error}`)
+      return;
+    }else{
+      console.log("Conexión extablecida con el servidor de MySQL")
+    }
+});
+
+module.exports =  {connection: connection}
--- backend-mi-proyecto-ppi/routes/actores.js
+++ backend-mi-proyecto-ppi/routes/actores.js
@@ -2,4 +2,15 @@ const { Router } = require("express")
 const router = Router()
 const {connection} = require('../db/mysql')
     
+router.get("/actores", (req, res) => {
+    connection.query('SELECT * FROM actores',  (error, rows, fields) => {
+        if(!error){
+            res.json(rows)
+        }else{
+            res.json({error: "Error ejecutando la consulta"})
+        }
+    })
+})
+
+  
 module.exports = router
